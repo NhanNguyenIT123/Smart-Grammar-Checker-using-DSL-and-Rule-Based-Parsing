3124a6f84711467bfc5d5e29bb1fc7e746e616a0
CMTrg <minhtruong2605@gmail.com>
2026-04-17 20:40:55 +0700

    feat(ui): add main grammar checker page and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "@/pages/HomePage";
 import AuthPage from "@/pages/AuthPage";
+import GrammarChecker from "./pages/GrammarChecker";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Route path="/login" element={<AuthPage />} />
           <Route path="/register" element={<AuthPage />} />
           <Route path="/forgot-password" element={<AuthPage />} />
+
+          <Route path="/grammar" element={<GrammarChecker />} />
         </Routes>
       </BrowserRouter>
     </>
